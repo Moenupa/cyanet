e8bc0cd79ff9154bca52f45fb988897f3dfbd595
Moenupa WANG <moenupa@outlook.com>
2024-04-01 16:46:36 -0400

    data loading

--- src/dataloading.py
+++ src/dataloading.py
@@ -4,7 +4,9 @@ from glob import glob
 import pandas as pd
 import torch
 from torchvision.io import read_image
+from torchvision.tv_tensors import Image
 from torchvision.transforms.v2 import (
+    Transform,
     Compose,
     RandomCrop,
     RandomResizedCrop,
@@ -15,49 +17,126 @@ from torchvision.transforms.v2 import (
 from torch.utils.data import Dataset
 
 
+DEBUG = False
+
+
+MAT_YUV2RGB = torch.tensor([[1.000, 0.000, 1.140],
+                            [1.000, -0.395, -0.581],
+                            [1.000, 2.032, 0.000]],
+                           dtype=torch.float32)
+
+MAT_RGB2YUV = torch.tensor([[0.299, 0.587, 0.114],
+                            [-0.147, -0.289, 0.436],
+                            [0.615, -0.515, -0.100]],
+                           dtype=torch.float32)
+
+
+class YUV2RGB(Transform):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __call__(self, x: Image) -> torch.Tensor:
+        if type(x) == dict:
+            return {k: self.yuv2rgb(v) for k, v in x.items()}
+        elif type(x) == Image or type(x) == torch.Tensor:
+            return self.yuv2rgb(x)
+        elif type(x) == list or type(x) == tuple:
+            return [self.yuv2rgb(v) for v in x]
+
+        assert False, f'no call script for type {type(x)}'
+
+    @staticmethod
+    def yuv2rgb(x: Image) -> Image:
+        v = torch.einsum('ij, jhw->ihw', MAT_YUV2RGB, x)
+        if DEBUG:
+            print('r:', v[0, :, :].min(), v[0, :, :].max(),
+                  'g:', v[1, :, :].min(), v[1, :, :].max(),
+                  'b:', v[2, :, :].min(), v[2, :, :].max())
+        return v.clamp(0, 1)
+
+
+class RGB2YUV(Transform):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __call__(self, x: Image | dict) -> Image | dict:
+        if type(x) == dict:
+            return {k: self.rgb2yuv(v) for k, v in x.items()}
+        elif type(x) == Image or type(x) == torch.Tensor:
+            return self.rgb2yuv(x)
+        elif type(x) == list or type(x) == tuple:
+            return [self.rgb2yuv(v) for v in x]
+
+        assert False, f'no call script for type {type(x)}'
+
+    @staticmethod
+    def rgb2yuv(x: Image) -> Image:
+        v = torch.einsum('ji, ihw->jhw', MAT_RGB2YUV, x)
+        if DEBUG:
+            print('y:', v[0, :, :].min(), v[0, :, :].max(),
+                  'u:', v[1, :, :].min(), v[1, :, :].max(),
+                  'v:', v[2, :, :].min(), v[2, :, :].max())
+        return v
+
+
+YUV_TRANSFORM = Compose([
+    ToDtype(torch.float32, scale=True),
+    RGB2YUV(),
+])
+
+
 DEFAULT_TRANSFORM = Compose([
     RandomCrop(size=(256, 256)),
     RandomHorizontalFlip(p=0.5),
-    # ToDtype(torch.float32, scale=True),
+    ToDtype(torch.float32, scale=True),
     # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 
 class LOLImageDataset(Dataset):
     def __init__(self, root: str = 'data/LOL-v2/Real',
-                 test: bool = True,
-                 transform: Compose = DEFAULT_TRANSFORM) -> None:
-        self.test: bool = test
+                 partition: str = 'test',
+                 transform: Compose = None,
+                 par_mapping: dict = {'test': 'Test', 'train': 'Train'},
+                 gt_mapping: dict = {'gt': 'Normal', 'lq': 'Low'}) -> None:
+        self.partition = partition
         self.transform = transform
 
-        self.images = self._find_images(root)
+        self.images = self._find_images(
+            f'{root}/{par_mapping[partition]}', gt_mapping)
 
     def __len__(self) -> int:
         return len(self.images)
 
-    def __getitem__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> dict[str, Image]:
         row = self.images.iloc[idx]
-        d = {k: read_image(v) for k, v in row.items()}
-        # return d
-        return self.transform(d)
+        d = {k: Image(read_image(v)) for k, v in row.items()}
 
-    @property
-    def par(self):
-        return 'Test' if self.test else 'Train'
+        if self.transform:
+            d = self.transform(d)
+        return d
 
-    def _find_images(self, root: str, mapping={'gt': 'Normal', 'lq': 'Low'}) -> pd.DataFrame:
+    def _find_images(self, root: str, gt_mapping: dict) -> pd.DataFrame:
         ret = pd.DataFrame()
-        for alias, label in mapping.items():
-            image_list = glob(f'{root}/{self.par}/{label}/*.png')
+        for alias, label in gt_mapping.items():
+            image_list = glob(f'{root}/{label}/*.png')
             updates = pd.Series(image_list,
                                 index=[os.path.basename(x) for x in image_list])
             ret[alias] = updates
+        assert ret.shape[0] > 0, f'No images found in {root}'
         return ret
+
+    def peek(self):
+        first = self[0]
+        print(first.shape, first.min(), first.max())
+        return first
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    dataset = LOLImageDataset(root='data/LOL-v2/Real', test=True)
+    dataset = LOLImageDataset(root='data/LOL-v2/Real',
+                              partition='test',
+                              transform=YUV_TRANSFORM)
     # (3, H, W)
     gt, lq = dataset[0].values()
     fig, axes = plt.subplots(1, 2)
--- src/misc.py
+++ src/misc.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from mpl_toolkits.mplot3d.axes3d import Axes3D
-from dataloading import LOLImageDataset
+from dataloading import LOLImageDataset, YUV_TRANSFORM, YUV2RGB, RGB2YUV
 from collections import Counter
 
 
@@ -11,40 +11,37 @@ def plot_image(*images: torch.Tensor, **labeled_images: torch.Tensor) -> None:
     for i, img in enumerate(images):
         labeled_images[f'Image {i+1}'] = img
 
-    fig, axes = plt.subplots(1, len(labeled_images), figsize=(12, 6))
+    rows, cols = len(labeled_images) // 2, 2
+    fig, axes = plt.subplots(rows, cols, figsize=(8, 6 * rows))
     axes: list[Axes] = [axes] if type(axes) == Axes else axes.flatten()
 
     for ax, (label, pixels) in zip(axes, labeled_images.items()):
-        # HW = [d for d in pixels.shape if d != 3]
-        # pixels = pixels.reshape(*HW, 3)
         pixels = pixels.permute(1, 2, 0)
-        print(pixels.shape)
         ax.imshow(pixels)
         ax.set_title(label)
 
+    plt.tight_layout()
     plt.show()
     return
 
 
-def plot_rgb(*images: np.ndarray, **labeled_images: np.ndarray) -> None:
+def plot_rgb(*images: torch.Tensor, **labeled_images: torch.Tensor) -> None:
     for i, img in enumerate(images):
         labeled_images[f'Image {i+1}'] = img
 
-    fig, axes = plt.subplots(1, len(labeled_images), figsize=(12, 6),
+    rows, cols = len(labeled_images) // 2, 2
+    fig, axes = plt.subplots(rows, cols, figsize=(8, 6 * rows),
                              subplot_kw={'projection': '3d'})
     axes: list[Axes3D] = [axes] if type(axes) == Axes3D else axes.flatten()
 
     for ax, (label, pixels) in zip(axes, labeled_images.items()):
-        pixels = np.asarray(pixels)
         assert 3 in pixels.shape, f'Expected 3 channels, got {pixels.shape}'
-
-        pixels = pixels.reshape(-1, 3)
+        pixels = pixels.reshape(-1, 3).numpy()
 
         color_counter = Counter(zip(pixels[:, 0], pixels[:, 1], pixels[:, 2]))
         R, G, B, freq = zip(*[(r, g, b, c)
                             for (r, g, b), c in color_counter.items()])
-        colors = np.array([R, G, B]).T
-
+        colors = torch.Tensor([R, G, B]).T
         ax.scatter(R, G, B, c=colors / 255, alpha=.1, s=freq)
         ax.set_xlabel('Red')
         ax.set_ylabel('Green')
@@ -55,11 +52,59 @@ def plot_rgb(*images: np.ndarray, **labeled_images: np.ndarray) -> None:
         ax.set_title(label)
         ax.invert_xaxis()
 
+    for ax in axes[1:]:
+        ax.shareview(axes[0])
+
+    plt.tight_layout()
     plt.show()
-    return
+
+
+def plot_yuv(*images: torch.Tensor, **labeled_images: torch.Tensor) -> None:
+    for i, img in enumerate(images):
+        labeled_images[f'Image {i+1}'] = img
+
+    rows, cols = len(labeled_images) // 2, 2
+    fig, axes = plt.subplots(rows, cols, figsize=(8, 6 * rows),
+                             subplot_kw={'projection': '3d'})
+    axes: list[Axes3D] = [axes] if type(axes) == Axes3D else axes.flatten()
+
+    for ax, (label, pixels) in zip(axes, labeled_images.items()):
+        assert 3 in pixels.shape, f'Expected 3 channels, got {pixels.shape}'
+        # (n, 3) in YUV space
+        yuv = YUV_TRANSFORM(pixels).flatten(1).numpy()
+
+        counter = Counter(zip(yuv[0, :], yuv[1, :], yuv[2, :]))
+        n_colors = len(counter)
+        Y, U, V, freq = zip(*[(y, u, v, c)
+                            for (y, u, v), c in counter.items()])
+        colors = YUV2RGB.yuv2rgb(
+            torch.Tensor([Y, U, V]).reshape(3, -1, 1)
+        ).flatten(1).permute(1, 0).numpy()
+        assert colors.shape == (n_colors, 3), f'{colors.shape} {n_colors}'
+        # assert not colors.shape, f'{colors.shape} {colors.min()} {colors.max()}'
+
+        ax.scatter(U, V, Y, c=colors, alpha=.1, s=freq)
+        ax.set_xlabel('U')
+        ax.set_ylabel('V')
+        ax.set_zlabel('Y')
+        ax.set_xlim(-0.436, 0.436)
+        ax.set_ylim(-0.615, 0.615)
+        ax.set_zlim(0, 1)
+        ax.set_title(label)
+        ax.invert_yaxis()
+
+    for ax in axes[1:]:
+        ax.shareview(axes[0])
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    dataset = LOLImageDataset()
-    # plot_rgb(**dataset[1])
+    dataset = LOLImageDataset(root='data/LOL',
+                              par_mapping={'test': 'eval15',
+                                           'train': 'our485'},
+                              gt_mapping={'gt': 'high', 'lq': 'low'})
+    plot_rgb(**dataset[0])
+    plot_yuv(**dataset[0])
     plot_image(**dataset[0])
